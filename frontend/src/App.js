16c4e1a4a062b751edd79e1984d21257f9b2be77
CoderAhmet79 <tahmet@yahoo.com>
2024-09-24 22:59:27 +0200

    some css rules were editted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Footer from './Components/Footer';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import AddNew from './Components/AddNew';
 import Stats from './Components/Stats'
+import Konjugation from './Components/Konjugation';
 
 
 
@@ -18,7 +19,7 @@ function App() {
 
   return (
 
-    <div className='flex flex-col justify-items-center bg-slate-50 h-svh m-0 p-0'>
+    <div className='flex flex-col justify-items-center drop-shadow-l h-svh m-0 p-0 min-w-fit'>
    
       <BrowserRouter>
         <Navbar/>
@@ -28,7 +29,8 @@ function App() {
             <Route exact path="/newlyadd" element={<Newest/>}/>
             <Route exact path="/phrases" element={<Phrases/>} />
             <Route exact path="/artikeltest" element={<ArtikelTest/>}></Route>   
-            <Route exact path="/new" element={<AddNew/>}></Route> 
+            <Route exact path="/new" element={<AddNew/>}></Route>
+            <Route exact path="/konj" element={<Konjugation/>}></Route> 
             <Route exact path="/stats" element={<Stats/>}></Route>     
           </Routes>     
       </BrowserRouter>
--- frontend/src/Components/Stats.js
+++ frontend/src/Components/Stats.js
@@ -58,6 +58,7 @@ const Stats = () => {
         </div>
         
       </div>
+      
     </div>
   );
 };
